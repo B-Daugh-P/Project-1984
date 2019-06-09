7857b0d2f08e19d9e930f5e36bbaaba5b814939f
Sviatoslav Nahirnyi <nsviat@gmail.com>
2019-06-09 16:14:51 +0300

    db added

---
+++ pco/DB_Service/DAOService.py
@@ -0,0 +1,51 @@
+import pymysql.cursors;
+
+
+class DAOService:
+    def __init__(self):
+        self.connection = pymysql.connect(host='localhost',
+                                          user='root',
+                                          password='',
+                                          db='counter',
+                                          charset='utf8mb4',
+                                          cursorclass=pymysql.cursors.DictCursor)
+
+    @staticmethod
+    def select_all(self):
+        try:
+            with self.connection.cursor() as cursor:
+
+                sql = "SELECT * FROM in_out "
+
+                cursor.execute(sql)
+
+                print("cursor.description: ", cursor.description)
+
+                print()
+
+                for row in cursor:
+                    print(row)
+
+        finally:
+            self.connection.close()
+
+    @staticmethod
+    def insert_in_out_people(self, in_out_param):
+
+            with self.connection as cursor:
+                sql = "INSERT INTO in_out VALUES(%s)"
+
+                DAOService.delete_all(DAOService())
+
+                cursor.execute(sql, in_out_param)
+
+    @staticmethod
+    def  delete_all(self):
+        with self.connection as cursor:
+            sql = "DELETE FROM in_out WHERE in_out.people_exists >= 0"
+
+            cursor.execute(sql)
+
+
+DAOService.insert_in_out_people(DAOService(), 10)
+DAOService.select_all(DAOService())
